3e2714c91c49e866df5fb43419340f3e91c250ee
Ericles Brum <ericlespbrum@gmail.com>
2026-07-27 20:57:50 -0300

    Validando a geração da pagina do swagger UI com Testcontainers e ASP.NET 10

--- rest-with-asp-net10-ericles/rest_with_asp_net10_ericles.Tests/IntegrationTests/SwaggerIntegrationTests.cs
+++ rest-with-asp-net10-ericles/rest_with_asp_net10_ericles.Tests/IntegrationTests/SwaggerIntegrationTests.cs
@@ -37,4 +37,21 @@ public class SwaggerIntegrationTests : IClassFixture<SqlServerFixture>
         content.Should().NotBeNull();
         content.Should().Contain("/api/person/v2");
     }
+
+    [Fact]
+    public async Task SwaggerUI_ShouldReturnSwaggerUI()
+    {
+        // Use current test cancellation token so test runner can cancel promptly
+        var ct = TestContext.Current.CancellationToken;
+
+        // Arrange & Act
+        var response = await _httpClient.GetAsync("/swagger-ui/index.html", ct);
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync(ct);
+        content.Should().NotBeNull();
+        content.Should().Contain("<div id=\"swagger-ui\">");
+    }
 }
