a7c413ccba6ae9fb0099de3f1a29d2aff33c6b12
Ericles Brum <ericlespbrum@gmail.com>
2026-07-27 17:15:54 -0300

    Criando a WebApplicationFactory para possibilitar testes de integracao

---
+++ rest-with-asp-net10-ericles/rest_with_asp_net10_ericles.Tests/IntegrationTests/Tools/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace rest_with_asp_net10_ericles.Tests.IntegrationTests.Tools;
+
+public class CustomWebApplicationFactory <TProgram>: WebApplicationFactory<TProgram> where TProgram : class
+{
+    private readonly string _connectionString;
+    public CustomWebApplicationFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                var dict = new Dictionary<string, string>
+                {
+                    {
+                        "MSSQLSERVERSQLConnection:MSSQLSERVERSQLConnectionString",
+                        _connectionString
+                    }
+                };
+                config.AddInMemoryCollection(dict!);
+            });
+        });
+    }
+}
