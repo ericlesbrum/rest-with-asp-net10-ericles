c4c462c73a73caefe4e2fd5549a13c5577b13abd
Ericles Brum <ericlespbrum@gmail.com>
2026-09-11 17:20:21 -0300

    Implementando o HyperMediaLink e o URLHelper responsaveis para prover HATEOAS

---
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Hypermedia/Filters/HypermediaFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace rest_with_asp_net10_ericles.Hypermedia.Filters;
+
+public class HypermediaFilter(HypermediaFilterOptions hypermediaFilterOptions) : ResultFilterAttribute
+{
+    private readonly HypermediaFilterOptions _hypermediaFilterOptions = hypermediaFilterOptions;
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        TryEnrichResult(context);
+        base.OnResultExecuting(context);
+    }
+
+    private void TryEnrichResult(ResultExecutingContext context)
+    {
+        if (context.Result is OkObjectResult objectResult)
+        {
+            var enricher = _hypermediaFilterOptions.ContentResponseEnricherList
+                .FirstOrDefault(option => option.CanEnrich(context));
+            enricher?.Enrich(context).Wait();
+        }
+    }
+}
---
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Hypermedia/Filters/HypermediaFilterOptions.cs
@@ -0,0 +1,8 @@
+﻿using rest_with_asp_net10_ericles.Hypermedia.Abstract;
+
+namespace rest_with_asp_net10_ericles.Hypermedia.Filters;
+
+public class HypermediaFilterOptions
+{
+    public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = [];
+}
