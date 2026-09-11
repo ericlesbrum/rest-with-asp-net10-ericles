c33ae851abdd875d67684af7cad386a3caefec9c
Ericles Brum <ericlespbrum@gmail.com>
2026-09-11 17:34:43 -0300

    Implementado o HyperMediaLink e o URLHelper Responsavel para prover HATEOAS

--- rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Hypermedia/HypermediaLink.cs
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Hypermedia/HypermediaLink.cs
@@ -1,5 +1,15 @@
-﻿namespace rest_with_asp_net10_ericles.Hypermedia;
+﻿using System.Xml.Serialization;
+
+namespace rest_with_asp_net10_ericles.Hypermedia;
 
 public class HypermediaLink
 {
+    [XmlAttribute]
+    public string Rel { get; set; } = string.Empty;
+    [XmlAttribute]
+    public string Href { get; set; } = string.Empty;
+    [XmlAttribute]
+    public string Type { get; set; } = "application/json";
+    [XmlAttribute]
+    public string Action { get; set; } = string.Empty;
 }
---
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Hypermedia/Utils/URLHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rest_with_asp_net10_ericles.Hypermedia.Utils;
+
+public static class URLHelper
+{
+    private static readonly object _lock = new object();
+    public static string BuildBaseURL(this IUrlHelper urlHelper, string routeName, string path)
+    {
+        lock (_lock)
+        {
+            var url = urlHelper.Link(routeName, new { controller = path }) ?? string.Empty;
+            if (url == null)
+                return string.Empty;
+            url = url.Replace("%2F", "/").TrimEnd('/');
+            return url;
+        }
+    }
+}
