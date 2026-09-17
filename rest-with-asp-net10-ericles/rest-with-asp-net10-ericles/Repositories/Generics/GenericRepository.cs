0dc47d454d33ff14aaeb2169f088d6f5828d2ba9
Ericles Brum <ericlespbrum@gmail.com>
2026-09-17 18:03:11 -0300

    Implementando PagedSearch no GenericRepository

--- rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Repositories/Generics/GenericRepository.cs
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Repositories/Generics/GenericRepository.cs
@@ -8,12 +8,12 @@ namespace rest_with_asp_net10_ericles.Repositories.Generics;
 public class GenericRepository<T> : IRepository<T> where T : BaseEntity
 {
     protected readonly MSSQLContext _context;
-    private readonly DbSet<T> dataset;
+    private readonly DbSet<T> _dataset;
 
     public GenericRepository(MSSQLContext context)
     {
         _context = context;
-        dataset = _context.Set<T>();
+        _dataset = _context.Set<T>();
     }
 
     public T Create(T item)
@@ -25,7 +25,7 @@ public class GenericRepository<T> : IRepository<T> where T : BaseEntity
 
     public bool Delete(long id)
     {
-        var existingItem = dataset.FirstOrDefault(item => item.Id == id);
+        var existingItem = _dataset.FirstOrDefault(item => item.Id == id);
         if (existingItem == null)
             return false;
         _context.Remove(existingItem);
@@ -35,26 +35,43 @@ public class GenericRepository<T> : IRepository<T> where T : BaseEntity
 
     public bool Exists(long id)
     {
-        return dataset.Any(item => item.Id == id);
+        return _dataset.Any(item => item.Id == id);
     }
 
     public List<T> FindAll()
     {
-        return dataset.ToList();
+        return _dataset.ToList();
     }
 
     public T FindById(long id)
     {
-        return dataset.FirstOrDefault(item=>item.Id == id)!;
+        return _dataset.FirstOrDefault(item=>item.Id == id)!;
     }
 
     public T Update(T item)
     {
-        var existingItem = dataset.FirstOrDefault(i => i.Id == item.Id);
+        var existingItem = _dataset.FirstOrDefault(i => i.Id == item.Id);
         if (existingItem == null)
             throw new KeyNotFoundException($"Item with id {item.Id} not found.");
         _context.Entry(existingItem).CurrentValues.SetValues(item);
         _context.SaveChanges();
         return item;
+    }
+
+    public List<T> FindWithPagedSearch(string query)
+    {
+        return _dataset.FromSqlRaw(query).ToList();
+    }
+
+    public int GetCount(string query)
+    {
+        using var connection = _context.Database.GetDbConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = query;
+
+        var result = Convert.ToInt32(command.ExecuteScalar());
+        return result;
     }
 }
--- rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Repositories/Interfaces/Generics/IRepository.cs
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Repositories/Interfaces/Generics/IRepository.cs
@@ -10,4 +10,6 @@ public interface IRepository<T> where T : BaseEntity
     T Update(T item);
     bool Delete(long id);
     bool Exists(long id);
+    public List<T> FindWithPagedSearch(string query);
+    public int GetCount(string query);
 }
