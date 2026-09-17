28bcfd6903e259f3d157c4b6628db9e7c2406e6a
Ericles Brum <ericlespbrum@gmail.com>
2026-09-17 17:44:45 -0300

    Criando PagedSearch

---
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Hypermedia/Utils/PagedSearchDTO.cs
@@ -0,0 +1,41 @@
+﻿using rest_with_asp_net10_ericles.Hypermedia.Abstract;
+
+namespace rest_with_asp_net10_ericles.Hypermedia.Utils;
+
+public class PagedSearchDTO<T> where T : ISupportsHyperMedia
+{
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalResults { get; set; }
+    public string SortedFields { get; set; }
+    public string SortDirecctions { get; set; } = "asc";
+    public Dictionary<string, object> Filters { get; set; } = [];
+
+    public List<T> List { get; set; } = [];
+
+    public PagedSearchDTO()
+    {
+    }
+
+    public PagedSearchDTO(int currentPage, int pageSize, string sortedFields, string sortDirections, Dictionary<string, object> filters)
+    {
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+        SortedFields = sortedFields;
+        SortDirecctions = sortDirections;
+        Filters = filters ?? [];
+    }
+
+    public PagedSearchDTO(int currentPage, string sortedFields, string sortDirections)
+    {
+        CurrentPage = currentPage;
+        PageSize = 10;
+        SortedFields = sortedFields;
+        SortDirecctions = sortDirections;
+        Filters = [];
+    }
+
+    public int GetCurrentPage() => CurrentPage <= 0 ? 1 : CurrentPage;
+
+    public int GetPageSize() => PageSize <= 0 ? 10 : PageSize;
+}
