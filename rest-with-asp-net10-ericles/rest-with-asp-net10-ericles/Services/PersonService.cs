a9e88068a9194729cf4b4abae4e437f6a90b42d0
Ericles Brum <ericlespbrum@gmail.com>
2026-09-18 16:28:52 -0300

    Implementando a geração dinâmica da query

--- rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Services/PersonService.cs
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Services/PersonService.cs
@@ -73,7 +73,24 @@ namespace rest_with_asp_net10_ericles.Services
         private (string query, string countQuery, string sort, int size, int offset) 
             BuildQueries(string name, string sortDirection, int pageSize, int page)
         {
-            throw new NotImplementedException();
+            page = Math.Max(1,page);
+
+            var  offset = (page - 1) * pageSize;
+            var size = pageSize < 1 ? 1 : pageSize;
+            var sort = (!string.IsNullOrWhiteSpace(sortDirection) && 
+                !sortDirection.Equals("desc", StringComparison.OrdinalIgnoreCase)) ? "asc" : "desc";
+            var whereClause = $" FROM Persons p WHERE 1=1 ";
+            if(!string.IsNullOrWhiteSpace(name))
+                whereClause += $" AND p.FirstName LIKE '%{name}%' ";
+
+            var query = $@"
+                SELECT * {whereClause}
+                ORDER BY p.FirstName {sort}
+                OFFSET {offset} ROWS FETCH NEXT {size} ROWS ONLY
+            ";
+            var countQuery = $"SELECT COUNT(*) {whereClause}";
+
+            return (query, countQuery, sort, size, offset);
         }
     }
 }
