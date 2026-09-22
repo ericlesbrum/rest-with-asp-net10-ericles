8a5877f7bb30f7105847a4c1c8581b9269ca2317
Ericles Brum <ericlespbrum@gmail.com>
2026-09-22 18:57:33 -0300

    Implementando o importer de XLSX - Lendo planilhas Excel

--- rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Files/Importers/XlsxImporter.cs
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Files/Importers/XlsxImporter.cs
@@ -1,5 +1,9 @@
-﻿using rest_with_asp_net10_ericles.Data.DTO.V2;
+﻿using ClosedXML.Excel;
+using CsvHelper;
+using CsvHelper.Configuration;
+using rest_with_asp_net10_ericles.Data.DTO.V2;
 using rest_with_asp_net10_ericles.Files.Importers.Contract;
+using System.Globalization;
 
 namespace rest_with_asp_net10_ericles.Files.Importers;
 
@@ -7,6 +11,28 @@ internal class XlsxImporter : IFileImporter
 {
     public Task<List<PersonDTO>> ImportFileAsync(Stream fileStream)
     {
-        throw new NotImplementedException();
+        using var workbook = new XLWorkbook(fileStream);
+        var worksheet = workbook.Worksheets.First();
+
+        var rows = worksheet.RowsUsed().Skip(1);
+
+        var people = new List<PersonDTO>();
+
+        foreach (var row in rows)
+        {
+            if (!row.Cell(1).IsEmpty())
+            {
+                var person = new PersonDTO
+                {
+                    FirstName = row.Cell(1).GetString(),
+                    LastName = row.Cell(2).GetString(),
+                    Address = row.Cell(3).GetString(),
+                    Gender = row.Cell(4).GetString(),
+                    Enabled = true
+                };
+                people.Add(person);
+            }
+        }
+        return Task.FromResult(people);
     }
 }
