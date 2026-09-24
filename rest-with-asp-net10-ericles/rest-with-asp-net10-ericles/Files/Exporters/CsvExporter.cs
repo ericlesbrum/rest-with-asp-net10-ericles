6e068c97f3e48ee45c4a08a9344d049095a09dcc
Ericles Brum <ericlespbrum@gmail.com>
2026-09-24 14:24:27 -0300

    Implementando o exportador de CSV

--- rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Files/Exporters/CsvExporter.cs
+++ rest-with-asp-net10-ericles/rest-with-asp-net10-ericles/Files/Exporters/CsvExporter.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Mvc;
 using rest_with_asp_net10_ericles.Data.DTO.V2;
 using rest_with_asp_net10_ericles.Files.Exporters.Contract;
+using System.Globalization;
+using System.Text;
 
 namespace rest_with_asp_net10_ericles.Files.Exporters
 {
@@ -8,7 +12,23 @@ namespace rest_with_asp_net10_ericles.Files.Exporters
     {
         public FileContentResult ExportFile(List<PersonDTO> persons)
         {
-            throw new NotImplementedException();
+            using var memoryStream = new MemoryStream();
+            using var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8, leaveOpen: true);
+
+            using var csv = new CsvWriter(streamWriter, new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            });
+
+            csv.WriteRecord(persons);
+            streamWriter.Flush();
+
+            var filesBytes = memoryStream.ToArray();
+
+            return new FileContentResult(filesBytes, MediaTypes.ApplicationCsv)
+            {
+                FileDownloadName = $"people_exported_{DateTime.UtcNow:ssmmHHddMMyyyy}.csv"
+            };
         }
     }
 }
